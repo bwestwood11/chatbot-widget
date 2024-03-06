fcedfe214355722b249922c917347d53ed1da225
bwestwood11 <dabrettwestwood@gmail.com>
2024-03-06 00:38:55 -0500

    Co-authored-by: Diwanshu Midha <Diwanshumidha@users.noreply.github.com> Co-authored-by: Raj kavankar <rajkavankar@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,250 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FaRocketchat } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import { FaMinus } from "react-icons/fa6";
+import { IoClose } from "react-icons/io5";
+import { LuDot } from "react-icons/lu";
+import { useEffect, useRef, useState } from "react";
+import { LuArrowUpRight } from "react-icons/lu";
+import { useThread } from "@/hooks/use-thread";
+import { useMessages } from "@/hooks/use-messages";
+import { cn } from "@/lib/utils";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { GoDotFill } from "react-icons/go";
+// import { FormError } from "../auth/form-error";
+// import useOnClickOutside from "@/hooks/useOnClickOutside";
+import { IAppProps } from "./main";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const BASEPATH = 'https://Chatty.com/api'
+
+const Widget = (props: IAppProps) => {
+  const [chatBox, setChatBox] = useState(false);
+  const { messages, setMessages } = useMessages();
+  const [userMessage, setUserMessage] = useState("");
+  const [showChat, setShowChat] = useState(false);
+  const { threadId, setThreadId } = useThread();
+  const [threadLoading, setThreadLoading] = useState(false);
+  const [generationLoading, setGenerationLoading] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+
+  // const scrollTriggerRef = useRef<>(null);
+  const messagesEndRef = useRef<HTMLDivElement | null>(null); // Ref for the element to scroll to
+  const widgetContainerRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // useOnClickOutside(widgetContainerRef, () => {
+  //   setChatBox(false);
+  // });
+
+  // Start a conversation/thread with the assistant
+  const createThread = async () => {
+    // show chat box
+    try {
+      setError("");
+      setChatBox(!chatBox);
+      if (threadId) {
+        setThreadId(threadId);
+        setShowChat(true);
+        setThreadLoading(false);
+        return;
+      }
+      setThreadLoading(true);
+
+      const response = await fetch(BASEPATH + "/run-assistant", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw Error("Error While Creating the chat");
+      }
+
+      if (!data.thread || !data.welcomeMessage) {
+        throw Error("Error While Creating the chat in welcome");
+      }
+      const thread: string = data.thread;
+      setThreadId(thread);
+      setShowChat(true);
+      setMessages((prev) => [
+        ...prev,
+        { from: "chatbot", message: data.welcomeMessage },
+      ]);
+    } catch {
+      setError(
+        "There Was an Error While Loading the ChatBot Refresh The Browser"
+      );
+    } finally {
+      setThreadLoading(false);
+    }
+  };
+
+  const handleUserMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setUserMessage("");
+    setGenerationLoading(true);
+
+    setMessages((prev) => [...prev, { from: "user", message: userMessage }]);
+    const response = await fetch(BASEPATH + "/answer-user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: userMessage, threadId: threadId }),
+    });
+    console.log(response);
+    const data = await response.json();
+    console.dir(data, {
+      depth: null,
+    });
+    setGenerationLoading(false);
+    setMessages((prev) => [...prev, { from: "chatbot", message: data }]);
+  };
+
+  const handleChatBox = () => {
+    setChatBox(!chatBox);
+  };
+
+  // calls the scrollToBottom function when the messages array changes to scroll to the bottom of the chat area
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="fixed bottom-5 right-5 p-3 z-[150]">
+      {/* The widget at the bottom right which starts a new thread onClick */}
+      <Button
+        onClick={() => createThread()}
+        className="bg-orange-500 p-7 justify-center flex items-center rounded-full hover:bg-orange-500/70"
+      >
+        {chatBox ? (
+          <IoClose className="w-8 h-8 text-white cursor-pointer" />
+        ) : (
+          <FaRocketchat className="w-8 h-8 text-white cursor-pointer" />
+        )}
+      </Button>
 
-export default App
+      {/* Parent div of the chat box */}
+      {chatBox && (
+        <div ref={widgetContainerRef} className="bg-white absolute mb-4 break-words flex flex-col bottom-full justify-between shadow-lg right-0 rounded-2xl w-96 h-[60dvh]">
+          {/* Start of orange header for chatbox */}
+          <div className="justify-between p-3 flex items-center bg-orange-500 rounded-t-2xl rounded-b-none text-white">
+            <div className="flex items-center">
+              <LuDot className="text-green-500 animate-pulse" size={40} />
+              <h2 className="text-lg font-bold text-center">
+                Chatty Assistant
+              </h2>
+            </div>
+            <div className="flex space-x-3">
+              <FaMinus
+                onClick={() => handleChatBox()}
+                className="cursor-pointer hover:text-black/60"
+              />
+              <IoClose
+                onClick={() => handleChatBox()}
+                className="cursor-pointer hover:text-black/60"
+              />
+            </div>
+          </div>
+          {threadLoading && messages.length <= 0 ? <WidgetLoader /> : null}
+          {error ? (
+            <div className=" px-4 py-2">
+              <p>{error}</p>
+            </div>
+          ) : null}
+          {/* Parent element for the chat area below orange header */}
+          <ScrollArea className="h-full w-full mt-6 space-y-2 py-2 text-sm">
+            {
+              messages.map((message, index) => (
+                <div
+                  key={`LoadingPointsWidget-${index}`}
+                  className={cn(
+                    "flex flex-1 px-4",
+                    message?.from === "user"
+                      ? "justify-end w-full"
+                      : "justify-start w-full"
+                  )}
+                >
+                  <div
+                    className={cn(
+                      "flex gap-y-1",
+                      message?.from === "user"
+                        ? "justify-end p-1.5 rounded-3xl w-3/4"
+                        : "justify-start w-3/4"
+                    )}
+                  >
+                    <span
+                      className={cn(
+                        message?.from === "user"
+                          ? "w-fit bg-orange-500 px-3 py-2 rounded-lg text-white text-end mb-2"
+                          : "w-fit bg-slate-100 px-3 py-2 text-start rounded-lg mb-2"
+                      )}
+                    >
+                      {message?.message}
+                    </span>
+                  </div>
+                </div>
+              ))}
+            {/* The loader for when the Assistant API is thinking of an answer */}
+            {generationLoading && (
+              <div className="px-4">
+                <p className="flex break-words py-1 text-start rounded-lg mb-2 w-1/3 text-slate-500 text-sm">
+                  {Array.from({ length: 3 }).map((_, index) => (
+                    <GoDotFill
+                      className={`animate-bounce delay-${index * 100} `}
+                      size={18}
+                    />
+                  ))}
+                </p>
+              </div>
+            )}
+            {/* This div is for scrolling to the bottom of the chat box when new messages appear */}
+            <div ref={messagesEndRef} />
+          </ScrollArea>
+          {/* The input that allows the user to chat to the assistant's API */}
+          {showChat && !threadLoading && (
+            <form
+              onSubmit={(e) => handleUserMessage(e)}
+              className="flex flex-row bg-white p-4"
+            >
+         
+              <input
+        type={"text"}
+        placeholder="Message..."
+        aria-label="Type here"
+        value={userMessage}
+                onChange={(e) => setUserMessage(e.target.value)}
+        className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 w-full flex justify-end items-end focus-visible:ring-transparent focus:ring-0 focus px-4 rounded-r-none text-sm"
+        {...props}
+      />
+              <Button type="submit" className="border-s-0" disabled={!threadId}>
+                <LuArrowUpRight size={25} />
+              </Button>
+            </form>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Widget;
+
+const WidgetLoader = () => {
+  return (
+    <div className=" w-full flex flex-col gap-2 mt-6 px-4 py-2">
+       <div
+      className={"animate-pulse rounded-md bg-primary/10 w-3/4 h-[24px] self-start"}
+  
+    />
+    
+    </div>
+  );
+};
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export interface IAppProps {
+  apiKey: string;
+}
+
+declare global {
+  interface Window {
+    Chatbot: {
+      mount: (el: Element, props: IAppProps) => void;
+    };
+  }
+}
+
+window.Chatbot = {
+  mount: function (el: Element, props: IAppProps) {
+    if (!props.apiKey) {
+      console.error("ERROR: apiKey is required for The Chatbot.");
+      ReactDOM.render(
+        <div style={{ color: "red" }}>
+          Please provide a valid apiKey to use the Chatbot. For More Information
+          see the <a href="#">Docs</a>
+        </div>,
+        el
+      );
+      return;
+    }
+    ReactDOM.render(<App {...props} />, el);
+  },
+};
+
+
+
+
+
