e9ab4688a0b8a2a8b683abbb14446c168111a0c2
bwestwood11 <dabrettwestwood@gmail.com>
2024-04-24 01:56:40 -0400

    updates to packages

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-// export const BASE_PATH = "https://www.chatbuild.io/api";
-export const BASE_PATH = "http://localhost:3000/api";
+
+export const BASE_PATH = process.env.NODE_ENV === "development" ? "http://localhost:3000/api" : "https://www.chatbuild.io/api";
